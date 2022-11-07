206d11ad2419df0cff7b79103674dc176575df60
xiaoyu-Aichiyu <lq20010610@163.com>
2022-11-07 14:18:33 +0800

    Initial commit

---
+++ src/main/java/com/qqls/youxiangousys/YouxiangouSysApplication.java
@@ -0,0 +1,13 @@
+package com.qqls.youxiangousys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouxiangouSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouxiangouSysApplication.class, args);
+	}
+
+}
