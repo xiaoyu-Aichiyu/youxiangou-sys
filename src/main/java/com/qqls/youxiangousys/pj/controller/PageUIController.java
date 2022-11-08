b7970d23af005b5642be8d38a137c8ffac5516a7
xiaoyu-Aichiyu <lq20010610@163.com>
2022-11-08 16:41:24 +0800

    初始化项目！

---
+++ src/main/java/com/qqls/youxiangousys/pj/controller/PageUIController.java
@@ -0,0 +1,30 @@
+package com.qqls.youxiangousys.pj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageUIController {
+
+    @RequestMapping("indexUI")
+    public String indexUI() {
+        return "index";
+    }
+
+    @RequestMapping("loginUI")
+    public String loginUI() {
+        return "login";
+    }
+
+    @RequestMapping("{module}/{page}")
+    public String moduleUI(@PathVariable("module") String module, @PathVariable("page") String page) {
+        //System.out.println("module = " + module + ", page = " + page);
+        return module + "/" + page;
+    }
+
+    @RequestMapping("pageUI")
+    public String pageUI() {
+        return "common/page";
+    }
+}
