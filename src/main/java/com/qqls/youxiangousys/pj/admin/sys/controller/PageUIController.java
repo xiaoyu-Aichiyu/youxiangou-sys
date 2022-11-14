ae9423065942bc76ce4e51e1d783ab62ded50e0a
cx <1802433928@qq.com>
2022-11-14 10:15:08 +0800

    2022.11.14 陈翔

--- src/main/java/com/qqls/youxiangousys/pj/admin/sys/controller/PageUIController.java
+++ src/main/java/com/qqls/youxiangousys/pj/admin/sys/controller/PageUIController.java
@@ -1,4 +1,4 @@
-package com.qqls.youxiangousys.pj.admin.controller;
+package com.qqls.youxiangousys.pj.admin.sys.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
